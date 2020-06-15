7a85658afa733ab7141ad64394978ba71b076b41
ijgnd <fooijgnd@barijgnd>
2020-06-15 07:21:18 +0200

    browser shortcut to copy copy nid,cid

--- src/browser.py
+++ src/browser.py
@@ -2,6 +2,9 @@ from aqt import gui_hooks
 from aqt.qt import (
     QAction,
     QApplication,
+    QKeySequence,
+    QShortcut,
+    qconnect,
 )
 from aqt.browser import Browser
 
@@ -20,3 +23,25 @@ def add_to_table_context_menu(browser, menu):
         na.triggered.connect(lambda _, nid=browser.card.nid: nidcopy(nid))
         menu.addAction(na)
 gui_hooks.browser_will_show_context_menu.append(add_to_table_context_menu)
+
+
+def browser_shortcut_helper_nid(browser):
+    if browser.card.nid:
+        nidcopy(browser.card.nid)
+
+
+def browser_shortcut_helper_cid(browser):
+    if browser.card.id:
+        cidcopy(browser.card.id)
+
+
+def set_shortcuts(browser):
+    ncombo = gc("shortcut: browser: copy nid")
+    ccombo = gc("shortcut: browser: copy cid")
+    if ncombo:
+        nidd_shortcut = QShortcut(QKeySequence(ncombo), browser)
+        qconnect(nidd_shortcut.activated, lambda b=browser:browser_shortcut_helper_nid(b))
+    if ccombo:
+        cidd_shortcut = QShortcut(QKeySequence(ccombo), browser)
+        qconnect(cidd_shortcut.activated, lambda b=browser:browser_shortcut_helper_cid(b))
+gui_hooks.browser_menus_did_init.append(set_shortcuts)
